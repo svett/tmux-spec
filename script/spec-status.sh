25f0f606068337b4a0275428323cb159004ee7b4
Svetlin Ralchev <svet@ralch.com>
2018-02-28 14:37:36 +0100

    Initial implementation

---
+++ script/spec-status.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+CURRENT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+PANE_DIR="$(tmux display-message -p -F "#{pane_current_path}")"
+
+# shellcheck disable=1090
+source "$CURRENT_DIR/shared.sh"
+
+print_status() {
+  color_fail=$(get_tmux_option "@tdd_status_color_fail" "#[bg=red] #[fg=black]")
+  color_pass=$(get_tmux_option "@tdd_status_color_pass" "#[bg=green] #[fg=black]")
+  color_none=$(get_tmux_option "@tdd_status_color_none" "#[bg=colour8] #[fg=gray]")
+
+  status_color=$color_none
+  status_message="N/A"
+
+  if [ -f "$PANE_DIR/.Guardfile" ]; then # [1]
+    exit_code=1
+
+    if [ "$exit_code" -eq 0 ]; then
+      status_color=${color_pass}
+      status_message="PASSING"
+    else
+      status_color=${color_fail}
+      status_message="FAILING"
+    fi
+  fi
+
+  printf "Spec: %s %s" "${status_color}" "${status_message}"
+}
+
+main() {
+  print_status
+}
+
+main
---
+++ spec-status.tmux
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# shellcheck disable=1090
+SPEC_STATUS_OUTPUT="#($CURRENT_DIR/script/spec-status.sh)"
+SPEC_STATUS_INTERPOLATION_STRING="\\#{spec_status}"
+
+# shellcheck disable=1090
+source "$CURRENT_DIR/scripts/shared.sh"
+
+do_interpolation() {
+  local string="$1"
+  local interpolated="${string/$SPEC_STATUS_INTERPOLATION_STRING/$SPEC_STATUS_OUTPUT}"
+  echo "$interpolated"
+}
+
+update_tmux_option() {
+  local option="$1"
+  local option_value
+  local new_option_value
+
+  option_value="$(get_tmux_option "$option")"
+  new_option_value="$(do_interpolation "$option_value")"
+
+  echo "$new_option_value"
+  set_tmux_option "$option" "$new_option_value"
+}
+
+main() {
+  update_tmux_option "status-right"
+  update_tmux_option "status-left"
+}
+
+main
